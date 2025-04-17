13c39aaa5996751b0e82bc726216c96c9904a04c
Minimax Femto <charlemax10@gmail.com>
2025-04-17 16:35:57 +0200

    commit 5 commentaires

--- src/CSon.cpp
+++ src/CSon.cpp
@@ -50,3 +50,41 @@ esp_err_t CSon::Setup()
     result = i2s_zero_dma_buffer(I2S_NUM_0); 
     return result;
 }
+
+esp_err_t CSon::SamplesDmaAcquisition() 
+{ 
+    // Nombre d'octets lus en mémoire DMA
+    size_t bytesRead; 
+
+    // Capture des données audio à partir de l'interface I2S
+    float result = i2s_read(I2S_NUM_0, &this->i2sData, sizeof(this->i2sData), &bytesRead, portMAX_DELAY); 
+
+    if (result == ESP_OK) 
+    { 
+        // Nombre d'échantillons audio extraits, chaque échantillon occupant 4 octets (format spécifique)
+        int16_t samplesRead = bytesRead / 4; 
+
+        if (samplesRead > 0)  
+        { 
+            float mean = 0; 
+
+            for (int16_t i = 0; i < samplesRead; ++i)  
+            { 
+                // Réduction de la précision de l'échantillon (décalage de bits pour ajustement en 16 bits)
+                i2sData[i] = i2sData[i] >> 8;  
+
+                // Calcul de la somme des valeurs absolues pour déterminer la moyenne du signal audio
+                mean += abs(i2sData[i]); 
+
+                // Mise à jour du niveau sonore crête si une valeur plus élevée est détectée
+                if (abs(i2sData[i]) > niveauSonoreCrete) 
+                    niveauSonoreCrete = abs(i2sData[i]); 
+            } 
+
+            // Calcul de la moyenne du niveau sonore en fonction des échantillons extraits
+            this->niveauSonoreMoyen = mean / samplesRead;  
+        }     
+    } 
+
+    return result; 
+} 

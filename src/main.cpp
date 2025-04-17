215a22b377005e0e1c6b15276c0a0f3576d52bab
Minimax Femto <charlemax10@gmail.com>
2025-04-17 16:59:49 +0200

    commit 6, teste de l'acquisition

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <Arduino.h>
 #include "SSD1306.h"
 #include "Wire.h"
+#include "CSon.h"
 
 SSD1306 ecranOled(0x3c, 5, 4);
+CSon son;
 
 void setup()
 {
+  Serial.begin(9600);
+
   ecranOled.init();
   ecranOled.clear();
   ecranOled.setFont(ArialMT_Plain_16);
   ecranOled.drawString(0, 10, "Mon Appli V1.0");
   ecranOled.display();
+
+  son.Setup();
 }
 
 void loop()
 {
-  
+  son.SamplesDmaAcquisition();
+  Serial.print("nvSonoreMoyen: ");
+  Serial.println(son.niveauSonoreMoyen);
+  sleep(1);
+  Serial.print("nvSonoreCrete: ");
+  Serial.println(son.niveauSonoreCrete);
+  sleep(1);
 }
